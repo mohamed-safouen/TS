db389cbec35e6a936406095c13c0af46025e5604
mohamed-safouen <mohamedsafouen@gmail.com>
2023-11-21 11:40:50 +0100

    (web/TS):Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 
 import './App.css';
+import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const PersonName = {
+    first: "Bruce",
+    last: "Wayne",
+  };
+  const nameList = [
+    { first: "Bruce", last: "Wayne" },
+    { first: "Clark", last: "Ken" },
+    { first: "Princess", last: "Diana" },
+  ];
   return (
     <div className="App">
-      
+      <Greet name="Vishwas" messageCount={10} isLoggedIn= {false}/>
+      <Person name={PersonName}/>
+      <PersonList names={nameList}/>
+
     </div>
   );
 }
